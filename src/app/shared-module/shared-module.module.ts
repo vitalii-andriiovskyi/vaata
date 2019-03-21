9bba8cab9acaaa803a54e4fc9353092fc1d24add
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-03-21 19:42:49 +0200

    reafactor: SharedModule: import MaterialModule

--- src/app/shared-module/shared-module.module.ts
+++ src/app/shared-module/shared-module.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule, MatInputModule, MatButtonModule, MatCheckboxModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  exports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule
   ]
 })
 export class SharedModuleModule { }
