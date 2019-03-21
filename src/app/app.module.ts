340846c7a584b3693e49aa12a6b8124aac83eaa2
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-03-21 19:05:26 +0200

    feat: SharedModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SharedModuleModule } from './shared-module/shared-module.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
